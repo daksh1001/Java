08867a8079a45167a1ae47e01a792569f1e19583
daksh1001 <49608636+daksh1001@users.noreply.github.com>
2020-02-22 16:09:26 +0530

    such logic is totally wrong
    I need to change whole code but answer is really true as you need.

--- Conversions/BinaryToOctal.java
+++ Conversions/BinaryToOctal.java
@@ -1,50 +1,36 @@
-package Conversions;
-
 import java.util.Scanner;
+public class BinaryOctal {
 
-/**
- * Converts any Binary number to an Octal Number
- *
- * @author Zachary Jones
- */
-public class BinaryToOctal {
-
-    /**
-     * Main method
-     *
-     * @param args Command line arguments
-     */
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input the binary number: ");
-        int b = sc.nextInt();
-        System.out.println("Octal equivalent: " + convertBinaryToOctal(b));
-        sc.close();
-
+    public static void main(String[] args) {
+        long binary;
+        
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter value of binary to convert it in octal");
+        binary=sc.nextLong();
+        int octal = convertBinarytoOctal(binary);
+        System.out.printf("%d in binary = %d in octal", binary, octal);
     }
 
-    /**
-     * This method converts a binary number to
-     * an octal number.
-     *
-     * @param binary The binary number
-     * @return The octal number
-     */
-    public static String convertBinaryToOctal(int binary) {
-        String octal = "";
-        int currBit = 0, j = 1;
-        while (binary != 0) {
-            int code3 = 0;
-            for (int i = 0; i < 3; i++) {
-                currBit = binary % 10;
-                binary = binary / 10;
-                code3 += currBit * j;
-                j *= 2;
-            }
-            octal = code3 + octal;
-            j = 1;
+    public static int convertBinarytoOctal(long binaryNumber)
+    {
+        int octalNumber = 0, decimalNumber = 0, i = 0;
+
+        while(binaryNumber != 0)
+        {
+            decimalNumber += (binaryNumber % 10) * Math.pow(2, i);
+            ++i;
+            binaryNumber /= 10;
         }
-        return octal;
-    }
 
+        i = 1;
+
+        while (decimalNumber != 0)
+        {
+            octalNumber += (decimalNumber % 8) * i;
+            decimalNumber /= 8;
+            i *= 10;
+        }
+
+        return octalNumber;
+    }
 }
